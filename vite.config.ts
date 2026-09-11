82120bd05f010d5984bd17dc4f25fc45c2e5c557
leongtay88 <leongtay@gmail.com>
2026-09-11 22:35:06 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/S1-7/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
